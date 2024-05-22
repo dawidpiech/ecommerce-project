5355df14c1bd1d06b317bd48e4e1ea679f6f0bc3
Dawid Piech <dawid.piech243@gmail.com>
2024-05-22 23:00:10 +0200

    Add product details components

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -3,12 +3,12 @@ import "./styles/globals.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { ProductsList } from "./views/ProductList/ProductList.jsx";
+import { ProductDetails } from "./views/ProductDetails/ProductDetails.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <ProductsList />,
+    element: <ProductDetails />,
   },
 ]);
 
