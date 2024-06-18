151c8eb54aceb98a36630f2d32d512249f466627
Dawid Piech <dawid.piech243@gmail.com>
2024-06-18 22:44:17 +0200

    Added the handling of cart

--- front-end/src/components/Layout/Layout.jsx
+++ front-end/src/components/Layout/Layout.jsx
@@ -8,30 +8,42 @@ import { MainContent } from "../MainContent/MainContent";
 import { MainMenu } from "../MainMenu/MainMenu";
 import { TopBar } from "../TopBar/TopBar";
 import { CurrencyContext } from "../../contexts/CurrencyContext";
-import { useState } from "react";
 import { CURRENCIES } from "../../constants/currencies";
+import { CartContext } from "../../contexts/CartContext";
+import { useLocalStorage } from "../../hooks/useLocalStorage";
 
 export function Layout() {
-  const [currency, setCurrency] = useState(
-    localStorage["selected_currency"] || CURRENCIES.PLN
+  const [currency, setCurrency] = useLocalStorage(
+    "selected_currency",
+    CURRENCIES.PLN
   );
+
+  const [cartItems, setCartItems] = useLocalStorage("cart_products", []);
+
+  function addProductToCart(product) {
+    const newState = [...cartItems, product];
+    setCartItems(newState);
+  }
+
   return (
     <>
-      <CurrencyContext.Provider value={[currency, setCurrency]}>
-        <MainContent>
-          <TopBar>
-            <MainMenu />
-            <Logo />
-            <div>
-              <CurrencySelector />
-              <IconMenu />
-            </div>
-          </TopBar>
-          <CategoryMenu />
-          <Outlet />
-        </MainContent>
-        <Footer />
-      </CurrencyContext.Provider>
+      <CartContext.Provider value={[cartItems, addProductToCart]}>
+        <CurrencyContext.Provider value={[currency, setCurrency]}>
+          <MainContent>
+            <TopBar>
+              <MainMenu />
+              <Logo />
+              <div>
+                <CurrencySelector />
+                <IconMenu />
+              </div>
+            </TopBar>
+            <CategoryMenu />
+            <Outlet />
+          </MainContent>
+          <Footer />
+        </CurrencyContext.Provider>
+      </CartContext.Provider>
     </>
   );
 }
